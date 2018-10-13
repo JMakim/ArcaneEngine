a47b500e69de4e97cf3619ffa96c959d789c9b59
Pavel Levin <pavlik_levin@outlook.com>
2018-10-12 20:32:18 -0400

    Skeleton files and Updated GDD
    Added skeleton cpp and h files for Arcane Engine App. Updated GDD, created logo and modified mechanics

---
+++ Source/ArcaneEngineApp/ArcaneEngineApp/Initialization.h
@@ -0,0 +1,51 @@
+//Detects what your system settings are by reading registry
+void detectHardware() {
+
+}
+
+
+//Checks what peripherals you have
+void checkInput() {
+
+}
+
+//Allocates memory variables and sets RAM
+void setMem() {
+
+}
+
+//Initializes cache
+void alocCache() {
+
+}
+
+//Sets up variables and methods for audio
+void setAudioBuffer() {
+
+}
+
+//Sets up graphical environment and stores graphical content
+void setGraphicsBuffer() {
+
+}
+
+//Creates app window
+void initWindow() {
+
+}
+
+//Creates audio playback manager
+void AudioListener() {
+
+}
+
+//checks for graphics files in resources
+void checkGraphicsResources() {
+
+}
+
+//Checks audio files in resources
+void checkAudioResources() {
+
+}
+
---
+++ Source/ArcaneEngineApp/ArcaneEngineApp/Resources.h
@@ -0,0 +1,10 @@
+//Loads audio and stores in an array, to be used by the audio listener
+void loadAudioResources() {
+
+}
+
+//Loads graphical content and renders it on screen
+void loadGraphicsResources() {
+
+}
+
