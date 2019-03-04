1b8ca6c74e10230abd8dc882cb53b6757fb6798d
Makim <jmakimramjahn@gmail.com>
2019-03-04 13:39:14 -0500

    engine now starts with main.cpp instead of ArcaneEngine.cpp
    reorganized file

---
+++ ArcaneEngine/Arcane.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+
+#ifndef Arcane_hpp
+#define Arcane_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+class Arcane
+{
+public:
+	static void Start();
+	static void Initialize();
+
+private:
+
+};
+
+
+
+
+
+#endif Arcane_hpp
---
+++ ArcaneEngine/Main.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include "Arcane.hpp"
+
+int main(int argc, char** argv) {
+	Arcane::Initialize();
+	
+	Arcane::Start();
+	
+	
+}
