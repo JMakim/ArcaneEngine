8b62b2fb35ed7636be52d003bd25e17f4bb5def1
Makim <jmakimramjahn@gmail.com>
2019-04-15 11:34:03 -0400

    last milestone
    here we go

---
+++ ArcaneEngine/Menu.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include <stdio.h>
+#include <tchar.h>
+
+#include <SFML/Graphics.hpp>
+#include <Windows.h>
+#include <SFML/System.hpp>
+#include <vector>
+#include <SFML/Audio.hpp>
+#include <iostream>
